dcbb76bfe22f170516d643966e00f81c34da2d66
Roberto <Roberto@Roberto_e_Alane>
2022-08-07 08:08:16 -0300

    Adicionando idade a classe Pessoa

--- projetoPrimeiro/src/curso/java/jdev1/Pessoa.java
+++ projetoPrimeiro/src/curso/java/jdev1/Pessoa.java
@@ -6,4 +6,18 @@ public class Pessoa {
 	private String sobrenome;
 	
 	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
 }
